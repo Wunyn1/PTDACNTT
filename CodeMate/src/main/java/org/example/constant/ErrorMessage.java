60a2d0918b7be2ff24b80fead4da31bc20fb4e14
maiHuong1311 <maihunw@gmail.com>
2026-05-02 22:09:35 +0700

    feat: create ErrorMessage

---
+++ CodeMate/src/main/java/org/example/constant/ErrorMessage.java
@@ -0,0 +1,11 @@
+package org.example.constant;
+
+public class ErrorMessage {
+    public static final String username = "Tên đăng nhập đã tồn tại!";
+    public static final String email = "Email không tồn tại!";
+    public static final String checkEmail = "Email này đã được sử dụng!";
+    public static final String checkPassword = "Mật khẩu không trùng khớp!";
+    public static final String  defeat = "Thất bại!";
+    public static final String fail = "Lỗi!";
+    public static final String  failLogin = "Sai tên đăng nhâp hoặc mật khẩu rồi bạn ơi!";
+}
