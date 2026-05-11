533395acebbc4eeea3e0d0487849942bc8f5b9f3
nahntt <nguyenthuyanh2401@gmail.com>
2026-05-11 12:01:56 +0700

    fix: update username_regex

--- CodeMate/src/main/java/org/example/utils/UsernameUtils.java
+++ CodeMate/src/main/java/org/example/utils/UsernameUtils.java
@@ -1,7 +1,7 @@
 package org.example.utils;
 
 public class UsernameUtils {
-    private static final String username_regex = "^[a-zA-Z0-9_-]{3,16}$";
+    private static final String username_regex = "^[a-zA-Z0-9._-]{3,16}$";
     public static boolean isValidUsername(String username) {
         return username.matches(username_regex);
     }
